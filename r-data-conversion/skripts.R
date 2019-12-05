54cdc16d9eaacb8963515f9d5fed6d917b4cabfd
kapsitis <kalvis.apsitis@gmail.com>
2019-12-05 22:40:45 +0200

    exported data to csv

--- r-data-conversion/skripts.R
+++ r-data-conversion/skripts.R
@@ -49,7 +49,6 @@ for (row in rowRange) {
 
 matchDF <- data.frame(respID = respID, emotion = emotion, shape = shape)
 exportJson <- toJSON(matchDF, matrix="rowmajor", pretty=TRUE)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 write(exportJson, file="match_emo_shp1.json")
 
 
@@ -120,9 +119,9 @@ rangeDF <- data.frame(respID = respID,
                       rInterGarl = rInterGarl,
                       rPatNepat = rPatNepat)
 exportJson <- toJSON(rangeDF, matrix="rowmajor", pretty=TRUE)
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 write(exportJson, file="match_shp_ranges_raw1.json")
 
+write.table(rangeDF, file="match_shp_ranges_raw1.csv", quote = TRUE,  col.names=TRUE, sep=",")
 
 
 
@@ -168,7 +167,7 @@ write(exportJson, file="match_aesth_shp1.json")
 
 
 ###################################################################
-## Produce "match_aesth_shp.json"
+## Produce "match_shp_emo2.json"
 ## Every shape is mapped to 1-3 aesthetical concepts.
 ###################################################################
 
@@ -201,4 +200,113 @@ shpEmoDF <- data.frame(respID = respID, shape = shape, emotion = emotion)
 exportJson <- toJSON(shpEmoDF, matrix="rowmajor", pretty=TRUE)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 write(exportJson, file="match_shp_emo2.json")
+
+
+###################################################################
+## Produce "match_aesth_shp.json"
+## Every shape is mapped to 1-3 aesthetical concepts.
+###################################################################
+
+rowRange <- 2:30
+# First 10 columns do not have emotion-shape matching data
+colOffset <- 241
+
+respID <- character(0)
+shape <- character(0)
+ranges <- character(0)
+rLaimNelaim <- character(0)
+rCerIzmis <- character(0)
+rUztrMier <- character(0)
+rBrivIerob <- character(0)
+rApmAizkait <- character(0)
+rInterGarl <- character(0)
+rPatNepat <- character(0)
+
+
+
+for (row in rowRange) {
+  for (jj in 1:(nShapes-4)) {
+    colN1 <- colOffset + (jj-1)*7 + 1
+    
+    rrr <- as.character(df[row,colN1])
+    
+    if (!is.na(df[row,colN1])) {
+      
+      print(sprintf("*** (%d,%d), frst = %s; colN = %d", row,jj, rrr, colN1))
+      theID <- sprintf("%s",df$Response.ID[row])
+      if (theID == "NA") {
+        theID <- sprintf("r%d",(row+1))
+      }
+      respID <- c(respID,theID)
+      
+      jjjj <- jj
+      if (jjjj > 1) {
+        jjjj <- jjjj+4
+      }
+      shape <- c(shape,sprintf("%d",jjjj))
+      ranges <- c(ranges, sprintf("%s,%s,%s,%s,%s,%s,%s",
+                                  as.character(df[row,colN1]),
+                                  as.character(df[row,colN1+1]),
+                                  as.character(df[row,colN1+2]),
+                                  as.character(df[row,colN1+3]),
+                                  as.character(df[row,colN1+4]),
+                                  as.character(df[row,colN1+5]),
+                                  as.character(df[row,colN1+6])))
+      rLaimNelaim <- c(rLaimNelaim,as.character(df[row,colN1]))
+      rCerIzmis <- c(rCerIzmis,as.character(df[row,colN1+1]))
+      rUztrMier <- c(rUztrMier,as.character(df[row,colN1+2]))
+      rBrivIerob <- c(rBrivIerob,as.character(df[row,colN1+3]))
+      rApmAizkait <- c(rApmAizkait,as.character(df[row,colN1+4]))
+      rInterGarl <- c(rInterGarl,as.character(df[row,colN1+5]))
+      rPatNepat <- c(rPatNepat,as.character(df[row,colN1+6]))
+    }
+  }
+}
+
+
+rangeDF2 <- data.frame(respID = respID, 
+                      shape = shape, 
+                      ranges = ranges,
+                      rLaimNelaim = rLaimNelaim,
+                      rCerIzmis=rCerIzmis,
+                      rUztrMier = rUztrMier,
+                      rBrivIerob = rBrivIerob,
+                      rApmAizkait = rApmAizkait,
+                      rInterGarl = rInterGarl,
+                      rPatNepat = rPatNepat)
+exportJson2 <- toJSON(rangeDF2, matrix="rowmajor", pretty=TRUE)
+write(exportJson2, file="match_shp_ranges_raw2.json")
+
+write.table(rangeDF2, file="match_shp_ranges_raw2.csv", quote = TRUE,  col.names=TRUE, sep=",")
+
+
+
+
+
+
+##########################################
+## HEATMAP
+##########################################
+
+pictureIDs <- c("1","6", "7", "8", "9", "10", "11", "13", "14", "15", "16", "17")
+scaleIDs <- c("rLaimNelaim", "rCerIzmis", "rUztrMier", "rBrivIerob", "rApmAizkait", "rInterGarl", "rPatNepat")
+mat <- matrix(rep(0,times=12*7), nrow = 12, ncol = 7, dimnames = list(pictureIDs,scaleIDs))
+
+mat[1,1] = 30
+
+remove.factors(rangeDF2)
+
+for (pictureID in pictureIDs) {
+  for (scaleID in scaleIDs) {
+    mat[scaleID, pictureID] <- sd(as.numeric(as.character(
+      rangeDF2[rangeDF2$shape==as.numeric(pictureID),scaleID])))
+  }
+}
+
+png(file = "heatmap-sd-shape-intervals-dataset2.png", bg = "transparent", type="cairo", units="in", width=5,  height=4, pointsize=12, res=96)
+pheatmap(t(mat), treeheight_row = 0, treeheight_col = 0, cluster_rows=F, cluster_cols=F)
+dev.off()
+
+
+
 
